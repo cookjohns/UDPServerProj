b545efb43966a75aca32beeaf76b48d1f595e85f
John Cook <jzc0066@auburn.edu>
2017-02-28 12:14:10 -0600

    Add computeChecksum methods

--- client.java
+++ client.java
@@ -2,4 +2,7 @@ import java.net.*;
 
 class Client {
 
+  private int computeChecksum() {
+    return 0;
+  }
 }
--- server.java
+++ server.java
@@ -2,4 +2,7 @@ import java.net.*;
 
 class Server {
 
+  private int computeChecksum() {
+    return 0;
+  }
 }

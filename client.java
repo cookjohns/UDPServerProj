0c951e55a76a845c7cce44f64f6fc67058f6d288
John Cook <jzc0066@auburn.edu>
2017-03-02 11:52:51 -0600

    Add sample code to client.java

--- client.java
+++ client.java
@@ -1,8 +1,22 @@
 import java.net.*;
+import java.io.*;
 
 // port #s 10008-10011
 
 class Client {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    DatagramSocket clientSocket = new DatagramSocket();
+    InetAddress ipAddr = InetAddress.getByName("hostname");
+
+    byte[] sendData    = new byte91024];
+    byte[] receiveData = new byte91024];
+
+    String sentence = input.readLine();
+
+    sendData = sentence.getBytes();
+  }
 
   private int computeChecksum() {
     return 0;

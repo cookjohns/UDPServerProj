234f292de7baddc811cd5acf848bb30e69d41b31
John Cook <jzc0066@auburn.edu>
2017-03-02 11:05:56 -0600

    Add port #s in comments

--- client.java
+++ client.java
@@ -1,5 +1,7 @@
 import java.net.*;
 
+// port #s 10008-10011
+
 class Client {
 
   private int computeChecksum() {
--- server.java
+++ server.java
@@ -1,5 +1,7 @@
 import java.net.*;
 
+// port #s 10008-10011
+
 class Server {
 
   private int computeChecksum() {

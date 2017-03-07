092eb3bc7adf589e3946721bdfdcc4f4ae6e3fb8
Alexei <azp0046@auburn.edu>
2017-03-07 11:44:42 -0600

    sample code working

--- client.java
+++ client.java
@@ -7,18 +7,37 @@ class Client {
 
   public static void main(String[] args) throws Exception {
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    
+    InetAddress ipAddr = InetAddress.getByName("localhost");
+    int portNumber = 10008;
+    
     DatagramSocket clientSocket = new DatagramSocket();
-    InetAddress ipAddr = InetAddress.getByName("hostname");
 
-    byte[] sendData    = new byte91024];
-    byte[] receiveData = new byte91024];
+    byte[] sendData    = new byte[1024];
+    byte[] receiveData = new byte[1024];
 
     String sentence = input.readLine();
 
     sendData = sentence.getBytes();
+    DatagramPacket sendPacket = new DatagramPacket(
+        sendData, sendData.length, ipAddr, portNumber);
+
+    clientSocket.send(sendPacket);
+
+    DatagramPacket receivePacket = new DatagramPacket(
+              receiveData, receiveData.length);
+
+
+    clientSocket.receive(receivePacket);
+    String modifiedSentence = new String(receivePacket.getData());
+
+    System.out.println("FROM SERVER:" + modifiedSentence);
+    clientSocket.close();
+
   }
 
   private int computeChecksum() {
     return 0;
   }
+
 }
--- server.java
+++ server.java
@@ -1,10 +1,39 @@
+import java.io.*;
 import java.net.*;
 
 // port #s 10008-10011
 
 class Server {
+	public static void main(String[] args) throws Exception
+	{
+		int portNumber = 10008;
+		DatagramSocket serverSocket = new DatagramSocket(portNumber);
+	
+		byte[] receiveData = new byte[1024];
+		byte[] sendData = new byte[1024];
 
-  private int computeChecksum() {
-    return 0;
-  }
+		while (true) {
+			DatagramPacket receivePacket = new DatagramPacket(
+					receiveData, receiveData.length);
+			serverSocket.receive(receivePacket);
+
+			String sentence = new String(receivePacket.getData());
+
+			InetAddress ipAddress = receivePacket.getAddress();
+			int clientPort = receivePacket.getPort();
+			String capitalizeSentence = sentence.toUpperCase();
+			sendData = capitalizeSentence.getBytes();
+
+			DatagramPacket sendPacket = new DatagramPacket(
+				sendData, sendData.length, ipAddress, clientPort);
+
+			serverSocket.send(sendPacket);
+
+		}
+
+	}
+
+	private int computeChecksum() {
+		return 0;
+	}
 }
